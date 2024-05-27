9d2029a0a8adaa9598b68c667c3d3718e5ae102e
victorsenfernandez <99834965+victorsenfernandez@users.noreply.github.com>
2024-05-27 16:41:21 +0200

    Update principal.cpp

--- src/principal.cpp
+++ src/principal.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include "tablero.h"
 
+
+using namespace std;
+
+
+int main() {
+
+	tablero m(8, 8);
+
+	/* salida en consola */
+	m.print(cout);
+	cout << "*****" << endl;
+
+	/* modificación de los valores */
+	m.cell(0,0,peon(color::BLANCO));
+
+
+	/* salida en consola */
+	m.print(cout);
+
+//	m[1][1].mueve(1, 1);
+	/* la memoria se libera adecuadamente? - C++ is awesome!*/
+
+
+}
